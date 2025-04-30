c869f0d4af7918b4bfe9cc9e1fe5b5d806b7b087
rkhodhairi <rkhodhairi@qumc.edu.sa>
2025-04-30 14:39:28 +0300

    Add project files.

---
+++ GlobalizationAndLocalizationTest/Program.cs
@@ -0,0 +1,61 @@
+using Microsoft.Extensions.Options;
+using System.Globalization;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+//builder.Services.AddControllersWithViews();
+
+// Add services to the container.
+builder.Services.AddLocalization(options => options.ResourcesPath = "Resources");
+
+builder.Services.AddControllersWithViews()
+    .AddViewLocalization(Microsoft.AspNetCore.Mvc.Razor.LanguageViewLocationExpanderFormat.Suffix)
+    .AddDataAnnotationsLocalization();
+
+builder.Services.Configure<RequestLocalizationOptions>(options =>
+{
+    var cultures = new List<CultureInfo> {
+        new CultureInfo("en"),
+        new CultureInfo("ar")
+    };
+    options.DefaultRequestCulture = new Microsoft.AspNetCore.Localization.RequestCulture("en");
+    options.SupportedCultures = cultures;
+    options.SupportedUICultures = cultures;
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+//var cultures = new List<CultureInfo> {
+//    new CultureInfo("en"),
+//    new CultureInfo("ar")
+//};
+//app.UseRequestLocalization(options => {
+//    options.DefaultRequestCulture = new Microsoft.AspNetCore.Localization.RequestCulture("en");
+//    options.SupportedCultures = cultures;
+//    options.SupportedUICultures = cultures;
+//});
+
+var localizationOptions = app.Services.GetRequiredService<IOptions<RequestLocalizationOptions>>().Value;
+app.UseRequestLocalization(localizationOptions);
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
